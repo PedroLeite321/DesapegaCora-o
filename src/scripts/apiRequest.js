ea2e185a9a0684eef215bd8e1b3e070d004efb4f
Pedro Leite <pk.bleite@gmail.com>
2024-05-22 07:25:30 -0300

    project start | Inicio do projeto

---
+++ src/scripts/apiRequest.js
@@ -0,0 +1,6 @@
+const apiRequestMain = () =>    {
+    const myApiConnector = () => {
+        const myApiKey = "xAqNDHxiSkL41H9pIIcBwiTmbEFAf5iAROwre8KzXPBR4Cknj0lpYTr2";
+        const myApiUrl = `https://api.pexels.com/v1/search?query=furniture`;
+    }
+}
