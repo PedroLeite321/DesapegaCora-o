4942e92aa1d5505b4155be0b92a252f82d9bdb1d
Pedro Kauã <pk.bleite@gmail.com>
2024-05-23 00:25:34 -0300

    css styling started | iniciando a entrevista.

--- src/scripts/apiRequest.js
+++ src/scripts/apiRequest.js
@@ -1,6 +1,42 @@
 const apiRequestMain = () =>    {
+    const states = {
+        pageInteractiveElements: {
+            userSeacher: document.querySelector("."),
+            nextImage_Right: document.querySelector("."),
+            nextImage_Left: document.querySelector("."),
+            submitButton: document.querySelector("")
+
+        },
+
+        pageViewElements: {
+            myModal: document.querySelector("."),
+            
+        }
+    };
+    state
     const myApiConnector = () => {
+
         const myApiKey = "xAqNDHxiSkL41H9pIIcBwiTmbEFAf5iAROwre8KzXPBR4Cknj0lpYTr2";
-        const myApiUrl = `https://api.pexels.com/v1/search?query=furniture`;
+        const myQuery = "";
+        const myApiUrl = `https://api.pexels.com/v1/search?query=${myQuery}&per_page=10`;
+
+        
+    }
+
+    const myFetch = (key, url, query) => {
+        fetch(url, {
+            headers: {
+                Authorization: key,
+            }
+        })
+        .then(response => response.json) 
+        .then(data => {
+            data.photos.forEach(photo => {
+                //I should probably base this on monitor screen.
+                const imgUrl = photo.src.medium;
+                alert(imgUrl);
+            });
+        })
+        .catch(error => console.error("Generic error: fetching images", error));
     }
 }
