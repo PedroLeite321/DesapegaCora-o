1f602711292ef1d21ce2261c9c3b14b2337e2380
Pedro Leite <pk.bleite@gmail.com>
2024-05-25 17:33:45 -0300

    Teste

--- src/scripts/apiRequest.js
+++ src/scripts/apiRequest.js
@@ -2,5 +2,12 @@ const apiRequestMain = () =>    {
     const myApiConnector = () => {
         const myApiKey = "xAqNDHxiSkL41H9pIIcBwiTmbEFAf5iAROwre8KzXPBR4Cknj0lpYTr2";
         const myApiUrl = `https://api.pexels.com/v1/search?query=furniture`;
+        
+        
+    }
+
+    const myFetcher = (key, url) => {
+        fetch(url)
+            .then()
     }
 }
